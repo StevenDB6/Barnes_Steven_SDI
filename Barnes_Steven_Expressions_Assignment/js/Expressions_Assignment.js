063eeb9e71154dac792bf878f741c4d8661e5989
Steven <stevendbarnes@comcast.net>
2015-05-14 12:12:17 -0500

    Created Expression files
    Created html and js in web storm

---
+++ Barnes_Steven_Expressions_Assignment/js/Expressions_Assignment.js
@@ -0,0 +1,9 @@
+/*
+Steven Barnes
+5/14/2015
+Section 01
+Barnes_Steven_Expressions_Assignment
+ */
+
+//alert("Testing to see if connected.");
+
--- Go to Training Week 2/JS/Week2.js
+++
@@ -1,8 +0,0 @@
-/*
-Steven Barnes
-5/14/2015
-Section 01
-GoTo Training Week 2
- */
-
-alert("Testing to see if connected")
