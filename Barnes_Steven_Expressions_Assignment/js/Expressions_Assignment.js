c4b836e9c92558b42e96794bee471ad25233491a
Steven <stevendbarnes@comcast.net>
2015-05-14 17:56:12 -0500

    Promoted welcome asked for name
    Promoted welcome asked for name

--- Barnes_Steven_Expressions_Assignment/js/Expressions_Assignment.js
+++ Barnes_Steven_Expressions_Assignment/js/Expressions_Assignment.js
@@ -7,3 +7,14 @@ Barnes_Steven_Expressions_Assignment
 
 //alert("Testing to see if connected.");
 
+// Create a weight calculator
+
+//Ask the user what is your name
+var name = prompt("Please type in your name:");
+// console log the name to check it
+console.log (name);
+
+// Alert the user and say welcome
+alert("Welcome "+ name+" Let's figure out how much you would weigh with body armour.");
+
+
