95b41d0a01e225f8c07c46e49094daeec2ccc30a
Steven <stevendbarnes@comcast.net>
2015-05-21 17:04:12 -0500

    Created array
    created piggyBank array and calculator to track quarters.

--- Barnes_Steven_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ Barnes_Steven_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -5,3 +5,20 @@ Section 01
 Barnes_Steven_Conditionals_Assignment
  */
 
+ // Create a calculator that keeps track of how much money is in a piggy bank
+
+// Create virtual piggy bank as an array
+var piggyBank = ["penny", "dime", "nickle", "quarter"];
+
+//print out the whole array
+console.log(piggyBank);
+
+//print out a specific coin use the index number
+console.log(piggyBank[0]);
+
+//How many items are inside of the array
+console.log(piggyBank.length);
+
+
+//create a variable to count the number of quarters in the array
+var totalNumQuarters=0; 
