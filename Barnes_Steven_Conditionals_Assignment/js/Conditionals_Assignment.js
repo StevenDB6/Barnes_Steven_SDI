97e77a81236062187e441059fed3a8b8561640a3
Steven <stevendbarnes@comcast.net>
2015-05-21 19:09:30 -0500

    Used a for loop
    To loop the the arrays to get the total number of pennies faster.

--- Barnes_Steven_Conditionals_Assignment/js/Conditionals_Assignment.js
+++ Barnes_Steven_Conditionals_Assignment/js/Conditionals_Assignment.js
@@ -8,7 +8,7 @@ Barnes_Steven_Conditionals_Assignment
  // Create a calculator that keeps track of how much money is in a piggy bank
 
 // Create virtual piggy bank as an array
-var piggyBank = ["penny", "dime", "nickle", "quarter", "penny"];
+var piggyBank = ["penny", "dimes", "nickles", "quarters","penny"];
 
 //print out the whole array
 console.log(piggyBank);
@@ -25,7 +25,7 @@ var totalNumQuarters=0;
 
 // Test each coin to see if it is a "quarter"
 
-if(piggyBank[0]==="quarter") {
+if(piggyBank[0]==="quarters") {
     //Code inside of here will run if that item is a quarter
     console.log("this item is a quarter");
     //add 1 to the total number of quarters
@@ -36,7 +36,7 @@ if(piggyBank[0]==="quarter") {
 }
 
 //Test second item in array
-if(piggyBank[1 ]==="quarter") {
+if(piggyBank[1 ]==="quarters") {
     //Code inside of here will run if that item is a quarter
     console.log("this item is a quarter");
     //add 1 to the total number of quarters
@@ -47,7 +47,7 @@ if(piggyBank[1 ]==="quarter") {
 }
 
 //Test third item
-if(piggyBank[3]==="quarter") {
+if(piggyBank[3]==="quarters") {
     //Code inside of here will run if that item is a quarter
     console.log("this item is a quarter");
     //add 1 to the total number of quarters
@@ -58,7 +58,7 @@ if(piggyBank[3]==="quarter") {
 }
 
 //Testing the forth item
-if(piggyBank[2]==="quarter") {
+if(piggyBank[2]==="quarters") {
     //Code inside of here will run if that item is a quarter
     console.log("this item is a quarter");
     //add 1 to the total number of quarters
@@ -71,14 +71,14 @@ if(piggyBank[2]==="quarter") {
 //Console log the total number of quarters
 console.log(totalNumQuarters+" quarters in the piggyBank");
 
-//create a variable to count the number of pennies in the array
-var totalNumPennies=0;
+//Create a variable to count the number of pennies in the array
+var totalNumPenny=0;
 
 if(piggyBank[3]==="penny") {
     //Code inside of here will run if that item is a penny
     console.log("this item is a penny");
     //add 1 to the total number of pennies
-    totalNumPennies++;
+    totalNumPenny++;
 }else{
     //code inside of here will run if the item in not a quater
     console.log("Item is not a penny");
@@ -89,27 +89,55 @@ if(piggyBank[0]==="penny") {
     //Code inside of here will run if that item is a penny
     console.log("this item is a penny");
     //add 1 to the total number of pennies
-    totalNumPennies++;
+    totalNumPenny++;
 }else{
     //code inside of here will run if the item in not a quater
     console.log("Item is not a penny");
 }
+
+//Added a second penny
 
 //Test the fifth item
 if(piggyBank[4]==="penny") {
     //Code inside of here will run if that item is a penny
     console.log("this item is a penny");
     //add 1 to the total number of pennies
-    totalNumPennies++;
+    totalNumPenny++;
 }else {
     //code inside of here will run if the item in not a quater
     console.log("Item is not a penny");
 }
 
 //Console log the total number of pennies and quarters
-console.log(totalNumPennies+" pennies in the piggyBank");
+console.log(totalNumPenny+" pennies in the piggyBank");
 console.log(totalNumQuarters+" quarters in the piggyBank");
 
-//Subtract a quater from the piggyBank
+//Cycling through arrays, For loop
 
-//
+var pennyTotal = 0;
+
+for(var i=0; i<piggyBank.length; i++){
+    //Code run each loop
+    console.log("the value of i is "+i);
+    //log each item in an array
+    console.log(piggyBank[i]);
+
+    //Test each item to see if is a penny
+    if(piggyBank[i]==="penny"){
+        console.log("item is a penny!");
+        //add one to total
+        pennyTotal++;
+    } else {
+        console.log("not a penny");
+    }
+
+}
+
+console.log("The total number of pennies in the piggy bank is "+ pennyTotal);
+
+
+/*
+I added another penny to get my results for the computer to calculate multiple pennies by creating a loop,
+and I got the total number of two.
+ */
+
