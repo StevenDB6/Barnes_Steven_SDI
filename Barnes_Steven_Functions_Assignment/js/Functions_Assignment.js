b16eb4cfd1847d0a850c21c7863c80e92bc10b04
Steven <stevendbarnes@comcast.net>
2015-05-28 20:12:55 -0500

    Prompter for kilometer per hour and miles per hour
    Prompter for kilometer per hour and miles per hour

--- Barnes_Steven_Functions_Assignment/js/Functions_Assignment.js
+++ Barnes_Steven_Functions_Assignment/js/Functions_Assignment.js
@@ -5,3 +5,30 @@ Section 01
 Barnes_Steven_Functions_Assignment
  */
 
+// Ask the user for current air speed
+var kmph = prompt("Lets figure out your current air speed in mph. What is your current speed in kmph.");
+
+//Validate the prompt
+while(kmph==="" || isNaN(kmph)) {
+    //Code will run if answerd with a number
+    //reprompt the user with if not true using same variable
+ if (isNaN(kmph)){
+        kmph = prompt(" Please only enter numbers to calculate.");
+
+    }
+
+}
+
+//Prompt user for mph
+
+var mph =prompt(" What does one kmph equal in mph.");
+
+//Validate the prompt
+while(mph==="" || isNaN(mph)){
+    //Code will run if true
+    //reprompt the user with same variable
+    if(isNaN(mph)){
+        mph = prompt(" Please only numbers to calculate.")
+    }
+}
+
